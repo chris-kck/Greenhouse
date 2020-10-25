3b2af445354117a5b40db479818e3975a0f7849f
Chris Kay <kckateera@gmail.com>
2020-10-25 22:48:01 +0200

    Add setup files

---
+++ setup.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+
+"""The setup script."""
+
+from setuptools import setup, find_packages
+
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
+
+with open('HISTORY.rst') as history_file:
+    history = history_file.read()
+
+requirements = ["dash", ]
+# production program requirements. eg ADC libraries, etc that i will use.
+
+setup_requirements = ['pytest-runner', ]
+
+test_requirements = ['pytest>=3', ]
+
+setup(
+    author="Kudzai Chris Kateera",
+    author_email='kckateera@gmail.com',
+    python_requires='>=3.6',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
+    description="Smart, Iot-enabled greenhouse monitoring and control Demo",
+    entry_points={
+        'console_scripts': [
+            'smartagro=smartagro.cli:main',
+        ],
+    },
+    install_requires=requirements,
+    license="GNU General Public License v3",
+    long_description=readme + '\n\n' + history,
+    include_package_data=True,
+    keywords='smartagro',
+    name='smartagro',
+    packages=find_packages(include=['']),
+    setup_requires=setup_requirements,
+    test_suite='tests',
+    tests_require=test_requirements,
+    url='https://github.com/chris-kck/greenhouse',
+    version='0.1',
+    zip_safe=False,
+)
